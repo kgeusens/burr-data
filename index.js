98dad7db28246a1218c57cc35b255335b8f904f2
Koen Geusens <koen_geusens@hotmail.com>
2023-04-13 15:30:58 +0200

    implemejnting destructuring

--- index.js
+++ index.js
@@ -96,11 +96,85 @@ export class Solution {
 
 export class Voxel {
 	#source
-	constructor(flatObject) {
+	#state=[]
+	"@attributes"={x:1, y:1, z:1, type:0}
+	text="_"
+	constructor(flatObject = {}) {
 		this.#source=flatObject
-		if (flatObject["@attributes"]) this["@attributes"] = flatObject["@attributes"]
-		if (flatObject.text) this.text = flatObject.text
+		if (!flatObject["@attributes"]) flatObject["@attributes"]={}
+		var { "@attributes" : {x=1, y=1, z=1, type=0, ...attrs}, text="_", ...props } = flatObject
+		this.x=x; this.y=y; this.z=z;
+		this.type=type
+		if (text.length != this.x*this.y*this.z) {text = this.text.repeat(this.x*this.y*this.z)}
+		this.setSize(this.x, this.y, this.z)
+		this.stateString = text
 	}
+	get x() { return this["@attributes"].x}
+	set x(v) { this.setSize(v, this.y, this.z); return v }
+	get y() { return this["@attributes"].y}
+	set y(v) { this.setSize(this.x, v, this.z); return v }
+	get z() { return this["@attributes"].z}
+	set z(v) { this.setSize(this.x, this.y, v); return v }
+	get type() { return this["@attributes"].type}
+	set type(v) { this["@attributes"].type = v; return v }
+	setSize(x, y, z) { 
+		this["@attributes"].x = x
+		this["@attributes"].y = y
+		this["@attributes"].z = z
+		for (let x=0;x<=this.x-1;x++) {
+			if (this.#state[x] == undefined) this.#state[x]=[];
+			for (let y=0;y<=this.y-1;y++) {
+				if (this.#state[x][y] == undefined) this.#state[x][y]=[];
+				for (let z=0;z<=this.z-1;z++) {
+					if (this.#state[x][y][z] == undefined) this.#state[x][y][z]={state:0}
+				}
+			}
+		}
+		this.text=this.stateString
+	}
+	get stateString() {
+		var ss = ""
+		for (let z=0;z<=this.z-1;z++) {
+			for (let y=0;y<=this.y-1;y++) {
+				for (let x=0;x<=this.x-1;x++) {
+					switch(this.#state[x][y][z].state) {
+						case 0:
+							ss+="_"
+							break;
+						case 1:
+							ss+="#"
+							break;
+						default:
+							ss+="+"
+					}
+				}
+			}
+		}
+		return ss
+	}
+	set stateString(s) {
+		let colorlessStateString = s.replace(/\d+/g,"")
+		this.text=colorlessStateString
+		let tv=0
+		for (let x=0;x<=this.x-1;x++) {
+			for (let y=0;y<=this.y-1;y++) {
+				for (let z=0;z<=this.z-1;z++) {
+					tv=colorlessStateString[x + y*this.x + z*this.x*this.y]
+					switch(tv) {
+						case "+":
+							this.#state[x][y][z].state=2
+							break;
+						case "#":
+							this.#state[x][y][z].state=1
+							break;
+						default:
+							this.#state[x][y][z].state=0
+					}
+				}
+			}
+		}
+	}
+	getVoxelPosition(x, y, z) { return this.#state[x][y][z]}
 }
 
 export class Shape {
@@ -202,5 +276,12 @@ export class Puzzle {
 		const xml = gunzipSync(xmpuzzle)
 		var result = parser.parse(xml)
 		return new Puzzle(result.puzzle)
+	}
+	getShape(idx) { return this.shapes.voxel[idx] }
+	addShape(obj) { 
+		let newVoxel = new Voxel(obj)
+		if (!this.shapes.voxel) this.shapes.voxel=[]
+		this.shapes.voxel.push(newVoxel); 
+		return this.shapes.voxel.length - 1
 	}
 }
--- test/test.js
+++ test/test.js
@@ -4,11 +4,17 @@ import { readFileSync} from 'fs'
 // Read a test xmpuzzle file and load it (this is the raw file from burrtools)
 const xmlFile = readFileSync("test.xmpuzzle");
 const thePuzzle = DATA.Puzzle.puzzleFromXMPuzzle(xmlFile)
-console.log(thePuzzle.saveToXML())
+//console.log(thePuzzle.saveToXML())
 // Read a plain text xml file and load it (in the xmpuzzle format)
 const xmpuzzleFile = readFileSync("test.xml");
 const theXMPuzzle = DATA.Puzzle.puzzleFromXML(xmpuzzleFile)
-console.log(theXMPuzzle.saveToXML())
-// Build an empty puzzle object
+//console.log(theXMPuzzle.saveToXML())
+// Build an empty puzzle object and test some methods
 const emptyPuzzle = new DATA.Puzzle()
+emptyPuzzle.addShape()
+emptyPuzzle.addShape({ "@attributes" : {x: 1, y: 2, z: 3}})
+console.log(emptyPuzzle.shapes.voxel[1])
+//emptyPuzzle.getShape(1).setSize(5, 2, 3)
+console.dir(emptyPuzzle, {depth: null})
 console.log(emptyPuzzle.saveToXML())
+
